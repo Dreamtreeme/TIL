600592b990b3d2e5e09c387010d7804aa31aa46c
victor_park <k99702@gmail.com>
2025-02-07 17:56:34 +0900

    d

--- Algorism/D2/1288. 새로운 불면증 치료법.py
+++ Algorism/D2/1288. 새로운 불면증 치료법.py
@@ -1,5 +1,19 @@
 T = int(input())
 
 for test_case in range(1, T + 1):
+
+    N = int(input())
+    count_num =1
+    count_list=[]
+    count_list_num=10
+    num2=0
+    while count_list_num>0:
+        num2 = N*count_num
+        count_list.append(num2%10)
+        num2 = N*count_num//10
+        if num2 ==0:
+            count_num+=1
+    
+
     result = 0
     print(f'#{test_case} {result}')

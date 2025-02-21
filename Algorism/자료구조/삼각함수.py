e098bfe312dd98a34356098c8948109e7637aed8
victor_park <k99702@gmail.com>
2025-02-21 11:23:24 +0900

    ㅇ

--- Algorism/자료구조/삼각함수.py
+++ Algorism/자료구조/삼각함수.py
@@ -8,3 +8,15 @@ x2,y2=(4,7)
 def calculate_distance(x1, y1, x2, y2):
   """두 점 사이의 거리를 계산하는 함수"""
   return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+# 3좌표중 목적구, 홀, 흰공
+# 흰공과 홀의 거리 a
+# 흰공과 목적구 거리 c
+# 목적구와 홀의 거리 b
+
+# 흰공과 목적구 각도 = 
+# math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b)))
+a=3
+b=4
+c=5
+math.degrees(math.acos((a**2 +b**2 -c**2))/(2*a*b))

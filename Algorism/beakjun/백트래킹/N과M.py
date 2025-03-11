f10cb8f1c37281d4168c9ad605555072c8963936
Victor_park <k99702@gmail.com>
2025-03-11 17:36:26 +0900

    c

--- Algorism/beakjun/백트래킹/N과M.py
+++ Algorism/beakjun/백트래킹/N과M.py
@@ -1,23 +1,19 @@
-def backtrack(sequence, visited, N, M):
-    # 수열의 길이가 M에 도달하면 출력
+def backtrack(sequence, visited, N, M,k):
+    
     if len(sequence) == M:
         print(" ".join(map(str, sequence)))
         return
-    
-    # 1부터 N까지의 숫자를 순회하며 순열 생성
-    for i in range(1, N + 1):
-        if not visited[i]:  # 중복 선택 방지
-            visited[i] = True  # 선택 표시
-            sequence.append(i)  # 수열에 추가
-            backtrack(sequence, visited, N, M)  # 다음 숫자 탐색
-            sequence.pop()  # 백트래킹: 선택 취소
-            visited[i] = False  # 백트래킹: 선택 해제
 
-# 입력 처리
+    for i in range(k, N+1):
+        if not visited[i]: 
+            visited[i] = True 
+            sequence.append(i)  
+            backtrack(sequence, visited, N, M,i+1)  
+            sequence.pop() 
+            visited[i] = False 
+
 N, M = map(int, input().split())
 
-# 방문 여부를 체크하는 리스트 (인덱스 1부터 사용)
 visited = [False] * (N + 1)
 
-# 백트래킹 시작
-backtrack([], visited, N, M)
+backtrack([], visited, N, M,1)

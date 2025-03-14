8f10e311a98882ff2d1ef6fa456171bba86e6100
Victor_park <k99702@gmail.com>
2025-03-14 17:34:29 +0900

    d

--- Algorism/DAY_homework/3월/5일/subtree.py
+++ Algorism/DAY_homework/3월/5일/subtree.py
@@ -1,23 +1,24 @@
+def dfs(node, tree):
+    cnt = 1
+    for child in tree[node]:
+        cnt += dfs(child, tree)
+    return cnt
 
-def treenode(n,tree):
-     
-    count = 1
-    for nextnode in tree[n]:
-        count += treenode(nextnode, tree)
-    return count
-
-T=int(input())
-
+T = int(input())
 for tc in range(1, T+1):
-    
-    E,N= map(int,input().split())
-    order=list(map(int,input().split()))
-    tree = [[] for _ in range(max(order)+1)]
-    for _ in range(E):
-         node=order.pop(0)
-         value=order.pop(0)
-         tree[node].append(value)
-    result =treenode(N,tree)
-    
+    E, N = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    # 트리의 크기를 필요한 만큼만 설정 (노드 번호가 가장 큰 값 + 1)
+    tree_size = max(arr) + 1  
+    tree = [[] for _ in range(tree_size)]  
+
+    # 리스트 인덱싱을 사용하여 O(E)로 트리 구성
+    for i in range(E):
+        parent = arr[2 * i]
+        child = arr[2 * i + 1]
+        tree[parent].append(child)
+
+    result = dfs(N, tree)
 
     print(f'#{tc} {result}')
